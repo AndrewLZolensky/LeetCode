eb9786804c48fd939ae345a27418783fe5d3faa9
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-11 01:37:59 -0500

    Time: 1 ms (99.98%) | Memory: 57.3 MB (65.13%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        
         // kadane algorithm
         int max = nums[0];
         int curr = 0;
-        
+
         for (int num : nums) {
             curr = curr > 0 ? curr + num : num;
             max = curr > max ? curr : max;
