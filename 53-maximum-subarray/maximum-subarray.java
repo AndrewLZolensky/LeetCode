0966c3c9f8ded7872d1e3c1e8565609eaba76c30
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-11 01:34:20 -0500

    Time: 1 ms (99.98%) | Memory: 56.8 MB (87.39%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int curr = 0;
+
+        for (int num : nums) {
+            if (curr >= 0) {
+                curr += num;
+            } else {
+                curr = num;
+            }
+
+            if (curr > max) {
+                max = curr;
+            }
+        }
+
+        return max;
+    }
+}
