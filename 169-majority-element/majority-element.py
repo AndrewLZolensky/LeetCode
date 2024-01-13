371929251cc0ff9d245840ff2a92bf3e2eb0d114
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-13 18:58:58 -0500

    Time: 113 ms (91.72%) | Memory: 14.7 MB (92.70%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,5 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return sorted(nums)[len(nums)/2]
+        cand = 0
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                cand = num
+            if cand == num:
+                count += 1
+            else:
+                count -= 1
+                
+        return cand
         
