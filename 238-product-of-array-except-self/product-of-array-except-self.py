5f6790e4b47a8cffe14f6efdcf78313f192b9e61
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-14 01:29:36 -0500

    Time: 173 ms (42.09%) | Memory: 23.8 MB (9.43%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -8,12 +8,10 @@ class Solution(object):
         a2 = [1]
         for i in range(0, len(nums)-1):
             a1.append(a1[i]*nums[i])
-            a2.append(1)
-        for i in range(1, len(nums)):
-            a2[-(i+1)] = a2[-i]*nums[-i]
+            a2.append(a2[i]*nums[-(i+1)])
         answer = []
         for i in range(0, len(nums)):
-            answer.append(a1[i]*a2[i])
+            answer.append(a1[i]*a2[-(i+1)])
 
         
         return answer
