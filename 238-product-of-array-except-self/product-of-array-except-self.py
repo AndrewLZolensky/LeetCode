210598706ba1ef0679311541dfb19d574b448dc3
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-14 01:35:30 -0500

    Time: 174 ms (39.80%) | Memory: 21.9 MB (34.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        a1 = [1]
+        answer = [1]
         a2 = [1]
         n = len(nums)
         for i in range(0, n-1):
-            a1.append(a1[i]*nums[i])
+            answer.append(answer[i]*nums[i])
             a2.append(a2[i]*nums[-(i+1)])
-        answer = []
         for i in range(0, n):
-            answer.append(a1[i]*a2[-(i+1)])
+            answer[i] = answer[i]*a2[-(i+1)]
 
-        
         return answer
         
