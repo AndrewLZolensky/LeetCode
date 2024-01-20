dbbf683407d9793721d6d62e9ce875a14cc380fa
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-19 19:47:19 -0500

    Time: 15 ms (95.59%) | Memory: 11.9 MB (100.00%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,33 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # take the list, split in half
-        # three cases: 1) drop happens at split, 2) left of split, 3) right of split
-        # if 1, the rightmost of left is bigger than leftmost of right
-        # if 2, rightmost smaller than leftmost for left list, opposite for right
-        # if 3, rightmost smaller than leftmost for right list, opposite for left
-
-        # Base cases:
-        # 1) if list has 1 number, return it
-        # 2) if list only has 2 numbers, return rightmost
-        # if list has 3 numbers, return min (2nd - 1st), (3rd - 2nd)
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        elif n == 2:
-            return min(nums[0], nums[1])
-        elif n == 3:
-            return min(nums[0], nums[1], nums[2])
-        else:
-            if nums[0] < nums[-1]:
-                return nums[0]
-            half = n // 2
-            left = nums[:half]
-            right = nums[half:]
-            if left[half - 1] > right[0]:
-                return right[0]
-            elif left[0] > left[half - 1]:
-                return self.findMin(left)
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            middle = (end + start) // 2
+            if (nums[middle] > nums[end]):
+                start = middle + 1
             else:
-                return self.findMin(right)
-        
+                end = middle
+        return nums[start]     
