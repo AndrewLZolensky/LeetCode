eb6b253c84328be51208dadc9fec8a39630390f6
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-01-19 20:05:38 -0500

    Time: 53 ms (55.64%) | Memory: 48.8 MB (5.29%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -5,8 +5,9 @@
 var findMin = function(nums) {
     let start = 0;
     let end = nums.length - 1;
+    let middle;
     while (start < end) {
-        let middle = Math.floor((start + end) / 2);
+        middle = Math.floor((start + end) / 2);
         if (nums[middle] < nums[end]) {
             end = middle;
         } else {
