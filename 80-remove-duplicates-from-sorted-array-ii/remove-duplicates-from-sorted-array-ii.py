e317b5fe7f243c7a757cbe3fc13824c082bf214a
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-10-29 19:47:43 -0400

    Time: 37 ms (25.84%) | Memory: 11.7 MB (15.39%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        write = 0
+        read = 0
+        prev1 = 0.5
+        prev2 = 0.5
+        while (read < len(nums)):
+            if (nums[read] == prev1) and (nums[read] == prev2):
+                pass
+            else:
+                nums[write] = nums[read]
+                write += 1
+            prev2 = prev1
+            prev1 = nums[read]
+            read += 1
+        return write
+        
