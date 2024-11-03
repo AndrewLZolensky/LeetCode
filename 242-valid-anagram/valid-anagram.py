6f0c7aecd9fd5d94d6cb62d59c2e9e8780dbc15c
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-11-03 14:24:10 -0500

    Time: 56 ms (7.74%) | Memory: 14 MB (6.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,23 +8,22 @@ class Solution(object):
         if (len(s) != len(t)):
             return False
 
-        s_freq = {}
-        t_freq = {}
+        freq = {}
         n = len(s)
         for i in range(n):
             s_sym = s[i]
             t_sym = t[i]
-            if s_sym in s_freq.keys():
-                s_freq[s_sym] += 1
+            if s_sym in freq.keys():
+                freq[s_sym] += 1
             else:
-                s_freq[s_sym] = 1
-            if t_sym in t_freq.keys():
-                t_freq[t_sym] += 1
+                freq[s_sym] = 1
+            if t_sym in freq.keys():
+                freq[t_sym] -= 1
             else:
-                t_freq[t_sym] = 1
-        for key in s_freq.keys():
-            num = s_freq[key]
-            if key not in t_freq.keys() or t_freq[key] != num:
+                freq[t_sym] = -1
+        for key in freq.keys():
+            num = freq[key]
+            if num != 0:
                 return False
         return True
         
