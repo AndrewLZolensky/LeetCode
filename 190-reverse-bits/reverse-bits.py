a091f6cd79bfc471fc20785a5b35326bb21ccf94
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-11-09 17:02:43 -0500

    Time: 17 ms (44.68%) | Memory: 11.7 MB (25.93%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -2,22 +2,11 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
-        if n == 0:
-            return 0
-        
-        counter = 0
         rev = 0
-        while n > 0:
-            mod = n % 2
-            rev |= mod
-            n //= 2
-
-            counter += 1
-            if counter < 32:
+        for i in range(32):
+            rev |= (n & 1)
+            if i < 31:
                 rev <<= 1
-        counter += 1
+                n >>= 1
+        return rev
 
-        while counter < 32:
-            rev <<= 1
-            counter += 1
-        return int(rev)
