57abdb9c48a4f46ff0e9b1237556ddfc9fd5882c
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-11-09 17:26:19 -0500

    Time: 15 ms (59.24%) | Memory: 13.7 MB (36.91%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        s = "".join([c for c in s if c.isalnum()])
+        n = len(s)
+        if n == 0 or n == 1:
+            return True
+
+        q = []
+        i1 = 0
+        i2 = 0
+        while (i2 < n - 1):
+            q.append(s[i1])
+            i1 += 1
+            i2 += 2
+        
+        if i2 == n-1:
+            i1 += 1
+        
+        while i1 < n:
+            compare = q.pop()
+            if s[i1] != compare:
+                return False
+            i1 += 1
+        
+        return True
+        
