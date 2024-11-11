1da0753b70972ff95b9da11651a423c5c3b1c034
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-11-11 01:18:05 -0500

    Time: 4 ms (25.49%) | Memory: 11.7 MB (41.08%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -10,20 +10,13 @@ class Solution(object):
         
         closed_to_open = {')': '(', '}': '{', ']': '['} # store pairings
         stack = []
+
         for c in s:
-            if len(stack) > 0: # catch case with no open bracket
-                last = stack[-1]
-            else:
-                last = None
-            if c in closed_to_open.keys(): # if closed
-                if last == closed_to_open[c]: # if match last parentheses, pop
-                    stack.pop()
-                else:
-                    return False # if the current parentheses is closed but not correct, return False
-            # if the current parentheses is open, add it
-            else:
+            if c in closed_to_open.values():
                 stack.append(c)
-        
-        if len(stack) > 0:
-            return False
-        return True
+            else:
+                if not stack or stack[-1] != closed_to_open[c]:
+                    return False
+                stack.pop()
+
+        return len(stack) == 0
