23e268787d39615755b641c84b4098e0af298cea
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2024-11-13 10:06:18 -0500

    Time: 0 ms (100.00%) | Memory: 12.2 MB (94.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        complements = {}
-        for i in range(len(nums)):
-            if nums[i] in complements.keys():
-                return complements[nums[i]], i
-            complements[target - nums[i]] = i
+        num_ix = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_ix:
+                return [num_ix[complement], i]
+            else:
+                num_ix[num] = i
+        return []
