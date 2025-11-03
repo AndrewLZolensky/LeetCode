89931680c8ddcb43f3649150ff2e5e13aad79251
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2025-11-03 08:17:49 -0500

    Time: 3 ms (4.23%) | Memory: 12.5 MB (54.36%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,26 +11,11 @@ class Solution(object):
         :type q: Optional[TreeNode]
         :rtype: bool
         """
-        flat_p = self.bfs(p)
-        flat_q = self.bfs(q)
-        if (len(flat_p) != len(flat_q)):
+        if p == None and q == None:
+            return True
+        if p == None:
             return False
-        for i in range(len(flat_p)):
-            if (flat_p[i] != flat_q[i]):
-                return False
-        return True
-    
-    def bfs(self, n):
-        flattened = list()
-        queue = deque()
-        queue.append(n)
-        while (len(queue) > 0):
-            curr = queue.popleft()
-            if (curr == None):
-                flattened.append(curr)
-            else:
-                flattened.append(curr.val)
-                queue.append(curr.left)
-                queue.append(curr.right)
-        return flattened
+        if q == None:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
