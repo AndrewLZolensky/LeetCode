83dcd95e2b14ce23af71f93de5f2f96478736222
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2025-11-04 14:40:57 -0500

    Time: 55 ms (15.82%) | Memory: 22.8 MB (18.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,18 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        answer = [1] * n
+        lp = [1]
+        for i in range(len(nums) - 1):
+            next_val = lp[-1] * nums[i]
+            lp.append(next_val)
+        rp = [1]
+        for i in range(1, len(nums)):
+            next_val = rp[-1] * nums[-i]
+            rp.append(next_val)
+        rp.reverse()
+        res = []
+        for i in range(len(lp)):
+            res.append(lp[i] * rp[i])
+        return res
 
-        leftmul = 1
-        for i in range(n-1):
-            leftmul *= nums[i]
-            answer[i+1] *= leftmul
-        
-        rightmul = 1
-        for i in range(1, n):
-            rightmul *= nums[-i]
-            answer[-(i+1)] *= rightmul
-
-        return answer
         
