475b84e88ca547dd39eac8a7f7c4847e9c5154fc
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2025-11-06 10:51:52 -0500

    Time: 39 ms (86.18%) | Memory: 22.1 MB (14.45%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,15 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxsum = float('-inf')
-        curr = 0
+        max_sum = nums[0]
+        curr_sum = 0
+        lo = 0
+        hi = 0
+        while hi < len(nums):
+
+            # increment current sum by number at hi
+            curr_sum += nums[hi]
+
+            # check if current sum is larger than previous max
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+
+            # incremement hi
+            hi += 1
+
+            # if curr sum is negative, clear it and move lo to hi
+            if curr_sum < 0:
+                lo = hi
+                curr_sum = 0
         
-        for num in nums:
-            if curr >= 0:
-                curr += num
-            else:
-                curr = num
-            if curr > maxsum:
-                maxsum = curr
-        
-        return(maxsum)
+        return max_sum
