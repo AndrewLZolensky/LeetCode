2eac376c9800ee062accd2574600241683c17934
Andrew Zolensky <115666750+AndrewLZolensky@users.noreply.github.com>
2025-11-06 11:16:45 -0500

    Time: 11 ms (57.20%) | Memory: 13.4 MB (8.86%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -4,12 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        L = [nums[0]]
-        S = [nums[0]]
+        l = nums[0]
+        s = nums[0]
+        m = nums[0]
         for i in range(1, len(nums)):
-            l = max([L[i-1] * nums[i], S[i-1] * nums[i], nums[i]])
-            L.append(l)
-            s = min([L[i-1] * nums[i], S[i-1] * nums[i], nums[i]])
-            S.append(s)
-        return max(L)
+            l_new = max(l * nums[i], s * nums[i], nums[i])
+            s_new = min(l * nums[i], s * nums[i], nums[i])
+            l = l_new
+            s = s_new
+            if l > m:
+                m = l
+        return m
+
         
